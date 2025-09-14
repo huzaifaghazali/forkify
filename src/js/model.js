97ae04acdafffa34acfe5475c9254310a946c5b8
huzaifaghazali <huzaifaghazali@gmail.com>
2025-09-14 20:52:00 +0500

    Add recipe model view components and integrate fracty for ingredient formatting

---
+++ src/js/model.js
@@ -0,0 +1,36 @@
+export const state = {
+  recipe: {},
+};
+
+const createRecipeObject = function (data) {
+  const { recipe } = data.data;
+  return {
+    id: recipe.id,
+    title: recipe.title,
+    publisher: recipe.publisher,
+    sourceUrl: recipe.source_url,
+    image: recipe.image_url,
+    servings: recipe.servings,
+    cookingTime: recipe.cooking_time,
+    ingredients: recipe.ingredients,
+  };
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    state.recipe = createRecipeObject(data);
+    console.log(state.recipe);
+  } catch (err) {
+    // Temp error handling
+    console.error(`${err} 💥💥💥💥`);
+    throw err;
+  }
+};

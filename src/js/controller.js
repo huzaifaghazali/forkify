856b7b7fab50b1ea3241c8a2f8e816b589a69b8d
huzaifaghazali <huzaifaghazali@gmail.com>
2025-09-15 18:35:40 +0500

    Refactor recipe view handling and improve error rendering

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,10 +5,8 @@ import recipeView from './views/recipeView.js';
 
 const recipeContainer = document.querySelector('.recipe');
 
-
 // NEW API URL (instead of the one shown in the video)
 // https://forkify-api.jonas.io
-
 
 ///////////////////////////////////////
 
@@ -25,14 +23,13 @@ const controlRecipes = async function () {
 
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
-   
   } catch (error) {
-    // alert(error);
+    recipeView.renderError();
     console.log(error);
   }
 };
 
-['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
-
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -7,6 +7,10 @@ class RecipeView {
   _errorMessage = 'We could not find that recipe. Please try another one!';
   _message = '';
 
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
+  }
+
   render(data) {
     this._data = data;
     const markup = this._generateMarkup();
@@ -20,6 +24,36 @@ class RecipeView {
           <svg>
             <use href="${icons}#icon-loader"></use>
           </svg>
+        </div>
+      `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderError(message = this._errorMessage) {
+    const markup = `
+        <div class="error">
+          <div>
+            <svg>
+              <use href="${icons}#icon-alert-triangle"></use>
+            </svg>
+          </div>
+          <p>${message}</p>
+        </div>
+      `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this._message) {
+    const markup = `
+        <div class="message">
+          <div>
+            <svg>
+              <use href="${icons}#icon-smile"></use>
+            </svg>
+          </div>
+          <p>${message}</p>
         </div>
       `;
     this._clear();

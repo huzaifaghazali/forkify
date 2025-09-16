3d8403dfc61699369f9e9e7c48130c490d4dc4cd
huzaifaghazali <huzaifaghazali@gmail.com>
2025-09-16 21:02:31 +0500

    Add pagination and results views, enhance search functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,8 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -31,7 +33,7 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
-
+    resultsView.renderSpinner();
 
     // 1) Get Search query
     const query = searchView.getQuery();
@@ -39,19 +41,29 @@ const controlSearchResults = async function () {
 
     // 2) Load search result
     await model.loadSearchResult(query);
-    console.log(model.state.search.results);
+    // console.log(model.state.search.results);
 
     // 3) Render results
+    resultsView.render(model.getSearchResultsPage());
 
-
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (error) {
-    console.log(err);
+    console.log(error);
   }
-}
+};
+
+const controlPagination = function (goToPage) {
+  // 1) Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
 
 const init = function () {
-
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -5,6 +5,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -50,10 +52,20 @@ export const loadSearchResult = async function (query) {
       };
     });
 
+    state.search.page = 1;
   } catch (error) {
     console.error(`${err} 💥💥💥💥`);
     throw err;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage; // 0
+  const end = page * state.search.resultsPerPage; // 9
+
+  return state.search.results.slice(start, end);
 };
 
 loadSearchResult('pizza');

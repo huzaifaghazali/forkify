d5937b88770ac238ca2f1b618397d77bfa845e55
huzaifaghazali <huzaifaghazali@gmail.com>
2025-09-18 21:36:04 +0500

    Implement servings update functionality

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -12,6 +12,15 @@ class RecipeView extends View {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
   }
 
+  addHandlerUpdateServings(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--update-servings');
+      if (!btn) return;
+      const { updateTo } = btn.dataset;
+      if (+updateTo > 0) handler(+updateTo);
+    });
+  }
+
   _generateMarkup() {
     return `
      <figure class="recipe__fig">
@@ -42,20 +51,24 @@ class RecipeView extends View {
           }</span>
           <span class="recipe__info-text">servings</span>
 
-          <div class="recipe__info-buttons">
-            <button class="btn--tiny btn--increase-servings">
-              <svg>
-                <use href="${icons}#icon-minus-circle"></use>
-              </svg>
-            </button>
-            <button class="btn--tiny btn--increase-servings">
-              <svg>
-                <use href="${icons}#icon-plus-circle"></use>
-              </svg>
-            </button>
-          </div>
-        </div>
-
+           <div class="recipe__info-buttons">
+             <button class="btn--tiny btn--update-servings" data-update-to="${
+               this._data.servings - 1
+             }">
+               <svg>
+                 <use href="${icons}#icon-minus-circle"></use>
+               </svg>
+             </button>
+             <button class="btn--tiny btn--update-servings" data-update-to="${
+               this._data.servings + 1
+             }">
+               <svg>
+                 <use href="${icons}#icon-plus-circle"></use>
+               </svg>
+             </button>
+           </div>
+         </div>
+ 
         <div class="recipe__user-generated">
           <svg>
             <use href="${icons}#icon-user"></use>

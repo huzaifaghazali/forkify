997c132a4e5b4b17d37fb2f9e10fe9313ef589ce
huzaifaghazali <huzaifaghazali@gmail.com>
2025-09-19 19:35:48 +0500

    Update bookmarks and results views for improved rendering and functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -23,10 +23,13 @@ const controlRecipes = async function () {
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
 
-    // 1) Loading recipe
+    // 1) Updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
+
+    // 2) Loading recipe
     await model.loadRecipe(id);
 
-    // 2) Rendering recipe
+    // 3) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (error) {
     recipeView.renderError();
@@ -81,13 +84,12 @@ const controlAddBookmark = function () {
   recipeView.update(model.state.recipe);
 
   // 3) Render bookmarks
-  // bookmarksView.render(model.state.bookmarks);
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
-
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
